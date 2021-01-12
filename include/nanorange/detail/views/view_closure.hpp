16d33c66c7869e57dd82ff8673eb5172eff1343d
Christopher Di Bella <cjdb.ns@gmail.com>
2021-01-11 19:47:51 -0800

    eliminates view_closure, vastly simplifies design

--- include/nanorange/detail/views/view_closure.hpp
+++ include/nanorange/detail/views/view_closure.hpp
@@ -9,183 +9,70 @@
 #include <type_traits>
 
 NANO_BEGIN_NAMESPACE
-	template<class F>
-	class range_adaptor;
+template<copy_constructible F>
+class range_adaptor;
 
-	template<semiregular F, copy_constructible... Args>
-	class view_closure;
+template<typename F, typename R, typename... Args>
+concept __pipeable =
+	viewable_range<R> and invocable<F, R, Args...> and view<std::invoke_result_t<F, R, Args...>>;
 
-	template<typename>
-	inline constexpr auto __is_view_closure_v = false;
+template<typename T, typename U>
+concept __decays_to = same_as<std::decay_t<T>, U>;
 
-	template<typename F, typename... Args>
-	inline constexpr auto __is_view_closure_v<view_closure<F, Args...>> = true;
+template<typename>
+inline constexpr auto __is_range_adaptor_v = false;
 
-	template<typename T>
-	concept __is_view_closure = __is_view_closure_v<std::remove_cvref_t<T>>;
+template<typename F>
+inline constexpr auto __is_range_adaptor_v<range_adaptor<F>> = true;
 
-	template<typename F, typename R, typename... Args>
-	concept __pipeable = viewable_range<R> and invocable<F, R, Args...> and view<std::invoke_result_t<F, R, Args...>>;
+template<typename T>
+concept __is_range_adaptor = __is_range_adaptor_v<std::remove_cvref_t<T>>;
 
-	template<semiregular F, copy_constructible... Args>
-	class view_closure {
-	public:
-		view_closure() = default;
+template<copy_constructible F>
+class range_adaptor : F {
+public:
+	constexpr range_adaptor(F&& f) noexcept(std::is_nothrow_move_constructible_v<F>)
+	: F(std::move(f))
+	{}
 
-		template<typename... Us>
-		constexpr explicit view_closure(F closure, Us&&... args)
-		: closure_(closure)
-		, args_(std::make_tuple(std::forward<Us>(args)...))
-		{}
-
-		template<input_range R>
-		requires __pipeable<F, R, Args...>
-		[[nodiscard]] constexpr auto operator()(R&& r) &
-		{
-			return apply(*this, std::forward<R>(r), std::index_sequence_for<Args...>());
-		}
-
-		template<input_range R>
-		requires __pipeable<F, R, Args...>
-		[[nodiscard]] constexpr auto operator()(R&& r) const&
-		{
-			return apply(*this, std::forward<R>(r), std::index_sequence_for<Args...>());
-		}
-
-		template<input_range R>
-		requires __pipeable<F, R, Args...>
-		[[nodiscard]] constexpr auto operator()(R&& r) &&
-		{
-			return apply(std::move(*this), std::forward<R>(r), std::index_sequence_for<Args...>());
-		}
-
-		template<input_range R>
-		requires __pipeable<F, R, Args...>
-		[[nodiscard]] constexpr auto operator()(R&& r) const&&
-		{
-			return apply(std::move(*this), std::forward<R>(r), std::index_sequence_for<Args...>());
-		}
-
-		template<input_range R, typename C>
-		requires same_as<std::remove_cvref_t<C>, view_closure> and __pipeable<F, R, Args...>
-		[[nodiscard]] constexpr friend auto operator|(R&& r, C&& closure)
-		{
-			return std::forward<C>(closure)(std::forward<R>(r));
-		}
-
-		template<__is_view_closure C1, __is_view_closure C2>
-		requires same_as<std::remove_cvref_t<C1>, view_closure>
-		[[nodiscard]] constexpr friend auto operator|(C1&& c1, C2&& c2)
-		{
-			return range_adaptor{
-				[x = std::forward<C1>(c1), y = std::forward<C2>(c2)]<viewable_range R>(R&& r) {
-					return std::invoke(y, std::invoke(x, std::forward<R>(r)));
-				},
-			};
-		}
-	private:
-		[[no_unique_address]] F closure_;
-		[[no_unique_address]] std::tuple<std::remove_cvref_t<Args>...> args_;
-
-		// @\expos@
-		template<class Self, class R, std::size_t... Is>
-		[[nodiscard]] static constexpr auto apply(Self&& self, R&& r, std::index_sequence<Is...>)
-		{
-			if constexpr (std::is_lvalue_reference_v<Self>) {
-				return std::invoke(self.closure_, std::forward<R>(r), get<Is>(self.args_)...);
-			}
-			else {
-				return std::invoke(std::move(self.closure_),
-				                   std::forward<R>(r),
-				                   std::move(get<Is>(self.args_))...);
-			}
-		}
-	};
-
-	template<semiregular F, copy_constructible... Args>
-	view_closure(F, Args&&...) -> view_closure<F, Args...>;
-
-	template<typename F1, typename F2>
-	auto compose_pipeline(range_adaptor<F1>, range_adaptor<F2>);
-
-	template<typename F, typename... Args>
-	auto compose_pipeline(range_adaptor<F> const& x, view_closure<Args...> const& y);
-
-	template<typename F, typename... Args>
-	auto compose_pipeline(view_closure<Args...> const& x, range_adaptor<F> const& y);
-
-	template<class F>
-	class range_adaptor : public F {
-	public:
-		constexpr range_adaptor(F&& f)
-		: F(std::move(f))
-		{}
-
-		template<input_range R, typename... Args>
-		requires invocable<F&, R, Args...>
-		[[nodiscard]] constexpr auto operator()(R&& r, Args&&... args) const
-		{
-			return std::invoke(static_cast<F const&>(*this), std::forward<R>(r), std::forward<Args>(args)...);
-		}
-
-		template<typename... Args>
-		[[nodiscard]] constexpr auto operator()(Args&&... args) const
-		{
-			return view_closure(static_cast<F const&>(*this), std::forward<Args>(args)...);
-		}
-
-		template<input_range R>
-		requires invocable<F&, R>
-		[[nodiscard]] constexpr friend auto operator|(R&& r, range_adaptor const& adaptor)
-		{
-			return adaptor(r);
-		}
-
-		template<typename F2>
-		[[nodiscard]] constexpr auto operator|(range_adaptor<F2> const& other) const
-		{
-			return compose_pipeline(*this, other);
-		}
-
-		template<typename... Args>
-		[[nodiscard]] constexpr friend auto operator|(range_adaptor const& x, view_closure<Args...> const& y)
-		{
-			return compose_pipeline(x, y);
-		}
-
-		template<typename... Args>
-		[[nodiscard]] constexpr friend auto operator|(view_closure<Args...> const& x, range_adaptor const& y)
-		{
-			return compose_pipeline(x, y);
-		}
-	};
-
-	template<typename F>
-	range_adaptor(F closure) -> range_adaptor<F>;
-
-	template<typename F1, typename F2>
-	auto compose_pipeline(range_adaptor<F1> x, range_adaptor<F2> y)
+	template<input_range R, typename... Args>
+	requires __pipeable<F&, R, Args...>
+	[[nodiscard]] constexpr auto operator()(R&& r, Args&&... args) const noexcept(std::is_nothrow_invocable_v<F&, R, Args...>)
 	{
-		return range_adaptor([x = std::move(x), y = std::move(y)]<viewable_range R>(R&& r) {
-			return std::invoke(y, std::invoke(x, std::forward<R>(r)));
-		});
+		return std::invoke(static_cast<F const&>(*this), std::forward<R>(r), std::forward<Args>(args)...);
 	}
 
-	template<typename F, typename... Args>
-	auto compose_pipeline(range_adaptor<F> const& x, view_closure<Args...> const& y)
+	template<typename... Args>
+	[[nodiscard]] constexpr auto operator()(Args&&... args) const
 	{
-		return range_adaptor([x = std::move(x), y = std::move(y)]<viewable_range R>(R&& r) {
-			return std::invoke(y, std::invoke(x, std::forward<R>(r)));
-		});
+		return nano::ranges::range_adaptor(
+		   [this, ...args = std::forward<Args>(args)]<viewable_range R>(R&& r) {
+			   return (*this)(std::forward<R>(r), args...);
+		   });
 	}
 
-	template<typename F, typename... Args>
-	auto compose_pipeline(view_closure<Args...> const& x, range_adaptor<F> const& y)
+	template<input_range R, __decays_to<range_adaptor> A>
+	requires __pipeable<F&, R>
+	[[nodiscard]] constexpr friend auto operator|(R&& r, A&& adaptor) noexcept(std::is_nothrow_invocable_v<F&, R>)
 	{
-		return range_adaptor([x = std::move(x), y = std::move(y)]<viewable_range R>(R&& r) {
-			return std::invoke(y, std::invoke(x, std::forward<R>(r)));
-		});
+		return std::forward<A>(adaptor)(std::forward<R>(r));
 	}
+
+	template<__decays_to<range_adaptor> T, __is_range_adaptor U>
+	[[nodiscard]] constexpr friend auto operator|(T&& x, U&& y)
+	{
+		return nano::ranges::range_adaptor(
+		   [x = std::forward<T>(x), y = std::forward<U>(y)]<viewable_range R>(R&& r) {
+			   return std::invoke(y, std::invoke(x, std::forward<R>(r)));
+		   });
+	}
+
+	friend void operator|(auto&&, range_adaptor const&&) = delete;
+	friend void operator|(range_adaptor const&&, auto&&) = delete;
+};
+
+template<typename F>
+range_adaptor(F closure) -> range_adaptor<F>;
 NANO_END_NAMESPACE
 
 #endif // NANORANGE_DETAIL_VIEWS_VIEW_CLOSURE_HPP
